9905dea92dbafa818b83b3608016c452ffba22b8
Nda-cunh <hydraldev@gmail.com>
2023-11-01 07:39:15 +0100

    Remove C_INCLUDE_PATH AND C_PLUS from profile generation

--- usr/post_install.sh
+++ usr/post_install.sh
@@ -6,7 +6,7 @@ if [ -f "$REPO_LIST" ]; then
 else
 	touch $REPO_LIST
 	echo "Cosmos https://gitlab.com/supraproject/suprastore_repository/-/raw/master/" | cat >> $REPO_LIST
-	echo "Elixir https://raw.githubusercontent.com/Strong214356/suprapack-list/master/" | cat >> $REPO_LIST
+	# echo "Elixir https://raw.githubusercontent.com/Strong214356/suprapack-list/master/" | cat >> $REPO_LIST
 fi
 
 CONFIG=$HOME/suprapack/user.conf
@@ -23,12 +23,6 @@ if ! grep -q 'export PATH=$PATH:$HOME/.local/bin' $HOME/.profile; then
 fi
 if ! grep -q 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib' $HOME/.profile; then
 	echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib' >> $HOME/.profile
-fi
-if ! grep -q 'export C_INCLUDE_PATH=$C_INCLUDE_PATH:$HOME/.local/include' $HOME/.profile; then
-	echo 'export C_INCLUDE_PATH=$C_INCLUDE_PATH:$HOME/.local/include' >> $HOME/.profile
-fi
-if ! grep -q 'export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$HOME/.local/include' $HOME/.profile; then
-	echo 'export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$HOME/.local/include' >> $HOME/.profile
 fi
 if ! grep -q 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/.local/share/pkgconfig' $HOME/.profile; then
 	echo 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/.local/share/pkgconfig' >> $HOME/.profile
