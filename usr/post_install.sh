248b22144e9a30319277d8ced07eab0b1e4c4119
Tidian Delage <tdelage@3C6.42angouleme.fr>
2023-11-01 06:45:52 +0100

    adding config file protection in post install script

--- usr/post_install.sh
+++ usr/post_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 REPO_LIST=$HOME/suprapack/repo.list
+CONFIG=$HOME/suprapack/user.conf
 
 if [ -f "$REPO_LIST" ]; then
 	echo "already existing repo.list skipping..."
@@ -8,4 +9,12 @@ else
 	touch $REPO_LIST
 	echo "Cosmos https://gitlab.com/supraproject/suprastore_repository/-/raw/master/" | cat >> $REPO_LIST
 	echo "Elixir https://raw.githubusercontent.com/Strong214356/suprapack-list/master/" | cat >> $REPO_LIST
+fi
+
+if [ -f "$CONFIG" ]; then
+	echo "already existing user.conf skipping..."
+else
+	touch $CONFIG
+	echo "Cosmos https://gitlab.com/supraproject/suprastore_repository/-/raw/master/" | cat >> $CONFIG
+	echo "Elixir https://raw.githubusercontent.com/Strong214356/suprapack-list/master/" | cat >> $CONFIG
 fi
